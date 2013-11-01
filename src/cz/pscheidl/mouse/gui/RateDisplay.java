c9a89f79c4ff32d47bea1d1cfc50c8b040e000c5
Pavel Pscheidl <pavelpscheidl@gmail.com>
2013-11-01 20:19:52 +0100

    Private modifier added to averageRate variable. Font and NumberFormat are now static. Static init added.

--- src/cz/pscheidl/mouse/gui/RateDisplay.java
+++ src/cz/pscheidl/mouse/gui/RateDisplay.java
@@ -13,48 +13,52 @@ import cz.pscheidl.mouse.util.MouseListener;
 
 /**
  * Counts average mouse rate and paints it actively.
- * 
+ *
  * @author Pavel Pscheidl
- * 
+ *
  */
 public class RateDisplay extends JPanel implements MouseListener {
-	private static final long serialVersionUID = -2920615231416789322L;
-	private static final int NANOSECONDS_IN_MILLISECOND = 1000000;
-	private static final int MILLISECONDS_IN_SECOND = 1000;
 
-	Font displayFont;
-	NumberFormat avgFormat;
+    private static final long serialVersionUID = -2920615231416789322L;
+    private static final int NANOSECONDS_IN_MILLISECOND = 1000000;
+    private static final int MILLISECONDS_IN_SECOND = 1000;
 
-	double averageRate = 0;
+    private static Font displayFont;
+    private static NumberFormat avgFormat;
 
-	public RateDisplay() {
-		setIgnoreRepaint(true);
-		displayFont = Settings.getMouseFont().deriveFont(36.0F);
-		avgFormat = new DecimalFormat("000.##");
-	}
+    private double averageRate = 0;
 
-	@Override
-	public void recieveMouseInfo(long[] mouseArray) {
+    static {
+        displayFont = Settings.getMouseFont().deriveFont(36.0F);
+        avgFormat = new DecimalFormat("000.##");
+    }
 
-		double delaySum = 0;
+    public RateDisplay() {
+        setIgnoreRepaint(true);
+    }
 
-		for (long delay : mouseArray) {
-			delaySum += delay;
-		}
+    @Override
+    public void recieveMouseInfo(long[] mouseArray) {
 
-		averageRate = MILLISECONDS_IN_SECOND
-				/ ((delaySum / mouseArray.length) / NANOSECONDS_IN_MILLISECOND);
-		paint(getGraphics());
+        double delaySum = 0;
 
-	}
+        for (long delay : mouseArray) {
+            delaySum += delay;
+        }
 
-	@Override
-	public void paint(Graphics g) {
-		g.setColor(Settings.getBgcolor());
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setColor(Color.WHITE);
-		g.setFont(displayFont);
-		g.drawString(avgFormat.format(averageRate), 0, 40);
-	}
+        averageRate = MILLISECONDS_IN_SECOND
+                / ((delaySum / mouseArray.length) / NANOSECONDS_IN_MILLISECOND);
+        paint(getGraphics());
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Settings.getBgcolor());
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.WHITE);
+        g.setFont(displayFont);
+        g.drawString(avgFormat.format(averageRate), 0, 40);
+    }
 
 }
