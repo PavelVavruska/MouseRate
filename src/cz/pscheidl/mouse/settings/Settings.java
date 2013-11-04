e996e7dc4a86f6407d65e25ab0b8c894ebce2e5b
Pavel Pscheidl <pavelpscheidl@gmail.com>
2013-11-04 07:37:00 +0100

    Code formatting changed.

--- src/cz/pscheidl/mouse/settings/Settings.java
+++ src/cz/pscheidl/mouse/settings/Settings.java
@@ -9,36 +9,36 @@ import javax.swing.ImageIcon;
 
 public class Settings {
 
-	private static final Color bgColor = new Color(30, 101, 134);
-	private static Font mouseFont = null;
+    private static final Color bgColor = new Color(30, 101, 134);
+    private static Font mouseFont = null;
 
-	private static final ImageIcon appIcon = new ImageIcon(Settings.class
-			.getClass().getResource("/cz/pscheidl/mouse/files/mouseIcon.png"));
+    private static final ImageIcon appIcon = new ImageIcon(Settings.class
+            .getClass().getResource("/cz/pscheidl/mouse/files/mouseIcon.png"));
 
-	public static ImageIcon getAppicon() {
-		return appIcon;
-	}
+    public static ImageIcon getAppicon() {
+        return appIcon;
+    }
 
-	public static Color getBgcolor() {
-		return bgColor;
-	}
+    public static Color getBgcolor() {
+        return bgColor;
+    }
 
-	public static Font getMouseFont() {
+    public static Font getMouseFont() {
 
-		if (mouseFont == null) {
-			try {
+        if (mouseFont == null) {
+            try {
 
-				mouseFont = Font.createFont(
-						Font.TRUETYPE_FONT,
-						Settings.class.getClass().getResourceAsStream(
-								"/cz/pscheidl/mouse/files/mouseFont.ttf"));
-				mouseFont = mouseFont.deriveFont(14.0f);
-			} catch (            FontFormatException | IOException e) {
-				e.printStackTrace();
-			}
-		}
+                mouseFont = Font.createFont(
+                        Font.TRUETYPE_FONT,
+                        Settings.class.getClass().getResourceAsStream(
+                                "/cz/pscheidl/mouse/files/mouseFont.ttf"));
+                mouseFont = mouseFont.deriveFont(14.0f);
+            } catch (FontFormatException | IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-		return mouseFont;
-	}
+        return mouseFont;
+    }
 
 }
